734e30542db4960257ae31827fbd5ed66180c607
root <root@ys-web01-vbo.alionis.net>
2011-11-07 19:00:31 +0100

    WSGI config ok

--- wsgi.py
+++ wsgi.py
@@ -1,13 +1,19 @@
-import os
+import os,sys
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "yaapp.settings")
 
 # This application object is used by the development server
 # as well as any WSGI server configured to use this file.
-from django.core.wsgi import get_wsgi_application
-application = get_wsgi_application()
 
-path = os.path.abspath(os.path.split(__file__)[0])
+#path = os.path.abspath(os.path.split(__file__)[0])
+path = '/home/meeloo/work/yaapp' #os.path.abspath(os.path.split(__file__)[0])
 if path not in sys.path:
     sys.path.append(path)
+
+import django.core.handlers.wsgi
+application = django.core.handlers.wsgi.WSGIHandler()
+
+#path = os.path.abspath(os.path.split(__file__)[0])
+#if path not in sys.path:
+#    sys.path.append(path)
 
