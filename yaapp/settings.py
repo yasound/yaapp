10db79959ed9e918e87a766599c694b6661358c8
Sebastien Metrot <meeloo@ys-web01-vbo.alionis.net>
2011-12-08 11:31:26 +0100

    vtenv + fab + wsgi config should now be ok, going to test it live on apache!

--- yaapp/settings.py
+++ yaapp/settings.py
@@ -59,23 +59,27 @@ USE_L10N = True
 
 # Absolute filesystem path to the directory that will hold user-uploaded files.
 # Example: "/home/media/media.lawrence.com/media/"
-MEDIA_ROOT = '/usr/local/lib/python2.6/dist-packages/django/contrib/admin/media'
+#MEDIA_ROOT = '/usr/local/lib/python2.6/dist-packages/django/contrib/admin/media'
 #MEDIA_ROOT = '/Users/mat/work/dev/yasound/yaapp/data/media'
+MEDIA_ROOT = os.path.join(PROJECT_PATH, 'media')
 
 # URL that handles the media served from MEDIA_ROOT. Make sure to use a
 # trailing slash.
 # Examples: "http://media.lawrence.com/media/", "http://example.com/media/"
-MEDIA_URL = 'http://yaapp.com/media/'
+MEDIA_URL = '/media/'
+ADMIN_MEDIA_PREFIX = '/media/admin/'
 
 # Absolute path to the directory static files should be collected to.
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/home/media/media.lawrence.com/static/"
-STATIC_ROOT = ''
+STATIC_ROOT = os.path.join(MEDIA_ROOT, 'statics')
+#STATIC_ROOT = ''
 
 # URL prefix for static files.
 # Example: "http://media.lawrence.com/static/"
-STATIC_URL = '/static/'
+#STATIC_URL = '/static/'
+STATIC_URL = '/media/statics'
 
 # URL prefix for admin static files -- CSS, JavaScript and images.
 # Make sure to use a trailing slash.
