a7865d5701b008d01ecf76af7287ffa05f7c49cb
Sebastien Metrot <sebastien@yasound.com>
2011-12-16 00:05:20 +0100

    fixed local double db

--- yaapp/settings.py
+++ yaapp/settings.py
@@ -32,6 +32,14 @@ if LOCAL_MODE:
             'PASSWORD': '',                  # Not used with sqlite3.
             'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
             'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
+        },
+        'yasound': {
+            'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
+            'NAME': os.path.join(PROJECT_PATH, 'db2.dat'),                      # Or path to database file if using sqlite3.
+            'USER': '',                      # Not used with sqlite3.
+            'PASSWORD': '',                  # Not used with sqlite3.
+            'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
+            'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
         }
     }
 else:
