6cf43d62d60b7f5d1a3a9f9ddc0a6c8adbbc749a
Jérôme Blondon <jblondon@bearstech.com>
2011-12-20 06:36:26 +0100

    Added basic test
    Added some basic models testing

--- yaapp/apps/yabase/tests.py
+++ yaapp/apps/yabase/tests.py
@@ -1,16 +1,63 @@
-"""
-This file demonstrates writing tests using the unittest module. These will pass
-when you run "manage.py test".
-
-Replace this with more appropriate tests for your application.
-"""
-
+from django.contrib.auth.models import User
 from django.test import TestCase
+from models import Radio
+from yabase.models import RadioUser
 
 
-class SimpleTest(TestCase):
-    def test_basic_addition(self):
-        """
-        Tests that 1 + 1 always equals 2.
-        """
-        self.assertEqual(1 + 1, 2)
+class TestModels(TestCase):
+    def setUp(self):
+        user = User(email="test@yasound.com", username="test", is_superuser=False, is_staff=False)
+        user.set_password('test')
+        user.save()
+        self.client.login(username="test", password="test")
+        self.user = user        
+    
+        # build some data
+        radio = Radio(creator=user, name='radio1')
+        radio.save()
+        self.radio = radio
+        
+    def testRadioUser(self):
+        connected = RadioUser.objects.get_connected()
+        self.assertEquals(len(connected), 0)
+        
+        ru = RadioUser(radio=self.radio, user=self.user)
+        ru.save()
+        
+        # test default fields
+        self.assertEquals(ru.mood, RadioUser.MOOD_NEUTRAL)
+        
+        # test favorite
+        favorites = RadioUser.objects.get_favorite()
+        self.assertEquals(len(favorites), 0)
+        
+        ru.favorite = True
+        ru.save()
+
+        favorites = RadioUser.objects.get_favorite()
+        self.assertEquals(len(favorites), 1)
+        
+        # test likers
+        likers = RadioUser.objects.get_likers()
+        self.assertEquals(len(likers), 0)
+        
+        ru.mood = RadioUser.MOOD_LIKE
+        ru.save()
+
+        likers = RadioUser.objects.get_likers()
+        self.assertEquals(len(likers), 1)
+
+        # test dislikers
+        dislikers = RadioUser.objects.get_dislikers()
+        self.assertEquals(len(dislikers), 0)
+        
+        ru.mood = RadioUser.MOOD_DISLIKE
+        ru.save()
+
+        dislikers = RadioUser.objects.get_dislikers()
+        self.assertEquals(len(dislikers), 1)
+
+        # misc fields
+        self.assertEquals(dislikers[0].radio, self.radio)
+        self.assertEquals(dislikers[0].user, self.user)
+        
