eca6e9fcdb0491b03461289cb67bb26cca034684
mat <mat@mat-MacBook-Pro-2.local>
2011-12-20 16:05:44 +0100

    playlist processing is now done in a task

--- yaapp/apps/yabase/task.py
+++ yaapp/apps/yabase/task.py
@@ -1,5 +1,6 @@
 import sys
 from celery.task import task
+from yabase.models import Radio, Playlist, SongMetadata, SongInstance
 
 
 @task
@@ -9,3 +10,74 @@ def test(a):
     f.write(a)
     f.close()
     print 'testing task done YAY!'
+    
+    
+@task
+def process_playlists(radio, lines):
+    print 'process_playlists'
+    
+    
+    print 'nb lines: %d' % len(lines)
+
+    ACTION_ADD_TAG = 'ADD'
+    ACTION_DEL_TAG = 'DEL'
+    PLAYLIST_TAG = 'LST'
+    ARTIST_TAG = 'ART'
+    ALBUM_TAG = 'ALB'
+    SONG_TAG = 'SNG'
+
+    action = None;
+    artist_name = None
+    album_name = None
+    playlist = None
+
+    for line in lines:
+        elements = line.split(';')
+        for i in range(len(elements)):
+            elements[i] = elements[i].strip('"')
+        tag = elements[0]
+        if tag == ACTION_ADD_TAG:
+            action = ACTION_ADD_TAG
+        elif tag == ACTION_DEL_TAG:
+            action = ACTION_DEL_TAG
+        elif tag == PLAYLIST_TAG:
+            playlist_name = elements[1]
+            source_name = 'test_playlist_file'
+            if action == ACTION_ADD_TAG:
+                playlist = Playlist.objects.create(name=playlist_name, source=source_name)
+                print 'playlist created: '
+                print playlist
+            elif action == ACTION_DEL_TAG:
+                try:
+                    playlist = Playlist.objects.get(name=playlist_name, source=source_name)
+                    playlist.delete()
+                    print 'playlist deleted: '
+                    print playlist
+                except Playlist.DoesNotExist:
+                    pass
+            
+        elif tag == ALBUM_TAG:
+            album_name = elements[1]
+        elif tag == ARTIST_TAG:
+            artist_name = elements[1]
+        elif tag == SONG_TAG:
+            order = int(elements[1])
+            song_name = elements[2]
+            if action == ACTION_ADD_TAG:
+                metadata = SongMetadata.objects.create(name=song_name, artist_name=artist_name, album_name=album_name)
+                song_instance = SongInstance.objects.create(playlist=playlist, metadata=metadata, song=0, order=order, play_count=0, yasound_score=0)
+            elif action == ACTION_DEL_TAG:
+                try: 
+                    metadata = SongMetadata.objects.get(name=song_name, artist_name=artist_name, album_name=album_name)
+                    metadata.delete()
+                    song_instance = SongInstance.objects.get(playlist=playlist, metadata=metadata, order=order)
+                    song_instance.delete()
+                except (SongMetadata.DoesNotExist, SongInstance.DoesNotExist):
+                    pass 
+                
+            
+            
+            
+            
+            
+            
--- yaapp/apps/yabase/views.py
+++ yaapp/apps/yabase/views.py
@@ -1,10 +1,10 @@
 from django.http import HttpResponse
 from django.shortcuts import get_object_or_404
 from django.views.decorators.csrf import csrf_exempt
-from yabase.models import Radio, Playlist, SongMetadata, SongInstance
+from yabase.models import Radio
 import zlib
 
-from yabase.task import test
+from yabase.task import test, process_playlists
 
 def test_task(request):
     print 'launch task\n'
@@ -17,50 +17,14 @@ def test_task(request):
 def upload_playlists(request, radio_id):
     radio = get_object_or_404(Radio, pk=radio_id)
 
-    print 'upload_playlists'
     print request.FILES
-    file = request.FILES['playlists_data']
-    content_compressed = file.read()
+    data = request.FILES['playlists_data']
+    content_compressed = data.read()
     content_uncompressed = zlib.decompress(content_compressed)
     lines = content_uncompressed.split('\n')
-    print 'nb lines: %d' % len(lines)
+    
+    print 'delay task process_playlists'
+    process_playlists.delay(radio, lines)
+    print 'delay task process_playlists 2'
 
-    ACTION_ADD_TAG = 'ADD'
-    ACTION_DEL_TAG = 'DEL'
-    PLAYLIST_TAG = 'LST'
-    ARTIST_TAG = 'ART'
-    ALBUM_TAG = 'ALB'
-    SONG_TAG = 'SNG'
-
-    action = None;
-    artist_name = None
-    album_name = None
-    playlist = None
-
-    for line in lines:
-        elements = line.split(';')
-        for i in range(len(elements)):
-            elements[i] = elements[i].strip('"')
-        tag = elements[0]
-        if tag == ACTION_ADD_TAG:
-            action = ACTION_ADD_TAG
-        elif tag == ACTION_DEL_TAG:
-            action = ACTION_DEL_TAG
-        elif tag == PLAYLIST_TAG:
-            playlist_name = elements[1]
-            source_name = 'test_playlist_file'
-            playlist = Playlist.objects.create(name=playlist_name, source=source_name)
-            print 'playlist created: '
-            print playlist
-        elif tag == ALBUM_TAG:
-            album_name = elements[1]
-        elif tag == ARTIST_TAG:
-            artist_name = elements[1]
-        elif tag == SONG_TAG:
-            order = int(elements[1])
-            song_name = elements[2]
-#            print "song: %(song)s - %(artist)s - %(album)s (%(order)d)" % {"song": song_name, "artist": artist_name, "album": album_name, "order": order}
-            metadata = SongMetadata.objects.create(name=song_name, artist_name=artist_name, album_name=album_name)
-            song_instance = SongInstance.objects.create(playlist=playlist, metadata=metadata, song=0, order=order, play_count=0, yasound_score=0)
-
-    return HttpResponse("You've successfully uploaded playlists to radio '%s'\n" % radio)
+    return HttpResponse("You've successfully launched playlists processing for radio '%s'\n" % radio)
