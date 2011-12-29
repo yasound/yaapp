b5efcff7e631713cf1e5674440b6fbf01beea7a9
mat <mat@mat-MacBook-Pro-2.local>
2011-12-29 17:26:05 +0100

    fixed account/admin

--- yaapp/apps/account/admin.py
+++ yaapp/apps/account/admin.py
@@ -2,7 +2,7 @@ from models import UserProfile
 from django.contrib import admin
 
 class UserProfileAdmin(admin.ModelAdmin):
-    list_display = ('user', 'twitter_account', 'facebook_account')
-    search_fields = ['user__username', 'twitter_account', 'facebook_account',]
+    list_display = ('user')
+    search_fields = ['user__username']
 admin.site.register(UserProfile)
 
