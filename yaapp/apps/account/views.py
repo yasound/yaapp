5f6c23dcd32ea222830d77d8ff4a63294cc07ef3
mat <campion.matthieu@gmail.com>
2012-01-02 17:47:39 +0100

    added missing view file

---
+++ yaapp/apps/account/views.py
@@ -0,0 +1,28 @@
+from django.http import HttpResponse
+from django.shortcuts import get_object_or_404
+from django.views.decorators.csrf import csrf_exempt
+from forms import UserPictureForm
+from models import User, UserProfile
+import datetime
+
+PICTURE_FILE_TAG = 'picture'
+
+@csrf_exempt
+def set_user_picture(request, user_id):
+    try:
+        user = User.objects.get(id=user_id)
+    except User.DoesNotExist:
+        return HttpResponse('user does not exist')
+    
+    if not request.FILES.has_key(PICTURE_FILE_TAG):
+        return HttpResponse('request does not contain a picture file')
+    
+    f = request.FILES[PICTURE_FILE_TAG]
+    d = datetime.datetime.now()
+    filename = unicode(d) + '.png'
+    
+    user.userprofile.picture.save(filename, f)
+    
+    res = 'picture OK for user: %s' % unicode(user)
+    return HttpResponse(res)
+    
