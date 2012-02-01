b6e9deb13dcab1335b51e84f9cf71e62e1f5a6b0
mat <campion.matthieu@gmail.com>
2012-02-01 17:19:29 +0100

    fixed stats api

--- yaapp/apps/stats/api.py
+++ yaapp/apps/stats/api.py
@@ -5,7 +5,6 @@ from tastypie import fields
 from models import RadioListeningStat
 from datetime import datetime, timedelta
 from django.db.models.aggregates import Max
-from Foundation import MAX
 
 class RadioListeningStatResource(ModelResource):
     radio = fields.ForeignKey('yabase.api.RadioResource', 'radio', full=False)
@@ -43,7 +42,9 @@ class RadioListeningStatResource(ModelResource):
                 daily_stat = RadioListeningStat(date=date, radio=hourly_stat.radio, connections=hourly_stat.connections, audience_peak=hourly_stat.audience_peak, overall_listening_time=hourly_stat.overall_listening_time, favorites=hourly_stat.favorites, likes=hourly_stat.likes, dislikes=hourly_stat.dislikes)
                 results.append(daily_stat)
             else:
-                audience_peak = MAX(daily_stat.audience_peak, hourly_stat.audience_peak)
+                audience_peak = daily_stat.audience_peak
+                if hourly_stat.audience_peak > audience_peak:
+                    audience_peak = hourly_stat.audience_peak
                 daily_stat.audience_peak = audience_peak
                 daily_stat.connections += hourly_stat.connections
                 daily_stat.overall_listening_time = hourly_stat.overall_listening_time
