4452b199493ee756e3104e1bf73fe6a170a1184d
mat <campion.matthieu@gmail.com>
2012-02-07 17:52:47 +0100

    scan friends every 2 hours

--- yaapp/settings.py
+++ yaapp/settings.py
@@ -363,7 +363,7 @@ CELERYBEAT_SCHEDULE = {
     },
     "scan_friends": {
         "task": "account.task.scan_friends_task",
-        "schedule": crontab(minute=0, hour='*'),
+        "schedule": crontab(minute=0, hour='*/2'),
     },
     "build-mongodb-index": {
         "task": "yaref.task.build_mongodb_index",
