01644cdaa82c097a5e5efe3034361e1aeaa26ca3
Jérôme Blondon <jerome@yasound.com>
2012-02-07 11:17:31 +0100

    Added missing file

---
+++ yaapp/apps/yabase/forms.py
@@ -0,0 +1,14 @@
+from django import forms
+from django.utils.translation import ugettext_lazy as _
+from models import Radio
+
+def get_genres():
+    genres = Radio.objects.all().values_list('genre', flat=True).distinct()
+    items = [(x, x) for x in genres]
+    items.insert(0, ('', '----------')) 
+    return items
+
+class SelectionForm(forms.Form):
+    genre =  forms.ChoiceField(label=_('Genre'), choices=get_genres(),
+                               required=False,
+                               widget=forms.Select(attrs={"placeHolder": _('Genre')}))
