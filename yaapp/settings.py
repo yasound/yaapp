7f1b01e32632e2f93213f7b18825f3eff76e4693
Jérôme Blondon <jerome@yasound.com>
2012-03-14 08:06:24 +0100

    mysql on local mode for yaapp too

--- yaapp/settings.py
+++ yaapp/settings.py
@@ -74,12 +74,18 @@ if LOCAL_MODE:
     else:
         DATABASES = {
             'default': {
-                'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
-                'NAME': os.path.join(PROJECT_PATH, 'db.dat'),                      # Or path to database file if using sqlite3.
-                'USER': '',                      # Not used with sqlite3.
-                'PASSWORD': '',                  # Not used with sqlite3.
-                'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
-                'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
+                'ENGINE': 'django.db.backends.mysql', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
+                'NAME': 'yaapp',                      # Or path to database file if using sqlite3.
+                'USER': 'root',                      # Not used with sqlite3.
+                'PASSWORD': 'root',                  # Not used with sqlite3.
+                'HOST': '127.0.0.1',                      # Set to empty string for localhost. Not used with sqlite3.
+                'PORT': '8889',                      # Set to empty string for default. Not used with sqlite3.
+#                'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
+#                'NAME': os.path.join(PROJECT_PATH, 'db.dat'),                      # Or path to database file if using sqlite3.
+#                'USER': '',                      # Not used with sqlite3.
+#                'PASSWORD': '',                  # Not used with sqlite3.
+#                'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
+#                'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
             },
             'yasound': {
                 'ENGINE': 'django.db.backends.mysql', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
