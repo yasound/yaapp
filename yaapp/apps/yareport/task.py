dab0db44ad829e78f50eb13015493ac00fb9d954
mat <campion.matthieu@gmail.com>
2012-03-16 16:16:31 +0100

    fixed song reporting duration: use duration from YasoundSong

--- yaapp/apps/yareport/task.py
+++ yaapp/apps/yareport/task.py
@@ -2,5 +2,5 @@ from celery.task import task
 from models import report_song
 
 @task
-def task_report_song(radio, song_instance, play_datetime, report_datetime):
-    report_song(radio, song_instance, play_datetime, report_datetime)
+def task_report_song(radio, song_instance):
+    report_song(radio, song_instance)
