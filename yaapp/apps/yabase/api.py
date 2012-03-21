051360022a4dd6b99069e540c309d3a3805a574d
Jérôme Blondon <jerome@yasound.com>
2012-03-21 21:43:26 +0100

    Enabled YasoundApiKeyAuthentication again

--- yaapp/apps/yabase/api.py
+++ yaapp/apps/yabase/api.py
@@ -85,7 +85,7 @@ class RadioResource(ModelResource):
         resource_name = 'radio'
         fields = ['id', 'name', 'creator', 'description', 'genre', 'theme', 'uuid', 'playlists', 'tags', 'favorites', 'audience_peak', 'overall_listening_time', 'created', 'ready']
         include_resource_uri = False;
-        #authentication = YasoundApiKeyAuthentication()
+        authentication = YasoundApiKeyAuthentication()
         authorization = Authorization()
         allowed_methods = ['get', 'put']
         filtering = {
