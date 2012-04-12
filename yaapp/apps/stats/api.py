1054cf8d65874ef25a630caff67436696c8dfb39
mat <campion.matthieu@gmail.com>
2012-04-12 14:57:49 +0200

    better daily_stats access

--- yaapp/apps/stats/api.py
+++ yaapp/apps/stats/api.py
@@ -6,6 +6,7 @@ from tastypie import fields
 from models import RadioListeningStat
 from datetime import datetime, timedelta
 from django.db.models.aggregates import Max
+from yabase.models import Radio
 
 class RadioListeningStatResource(ModelResource):
     radio = fields.ForeignKey('yabase.api.RadioResource', 'radio', full=False)
@@ -26,34 +27,14 @@ class RadioListeningStatResource(ModelResource):
         ]
 
     def get_object_list(self, request):
-        radio = int(request.GET.get('radio', 0))
-        ref = datetime.now() - timedelta(days=30)
-        original_stats = super(RadioListeningStatResource, self).get_object_list(request).filter(date__gt=ref)
-        if radio > 0:
-            original_stats = original_stats.filter(radio=radio)
-        original_stats = original_stats.order_by('date')
+        radio_id = int(request.GET.get('radio', 0))
+        try:
+            radio = Radio.objects.get(id=radio_id)
+        except Radio.DoesNotExist:
+            return []
         
-        results = []
-        # create RadioListeningStat for each day, but DON'T SAVE IT !!! it must not be added in the database
-        date = None
-        daily_stat = None
-        for hourly_stat in original_stats: 
-            if not date or date.date() != hourly_stat.date.date():
-                date = hourly_stat.date
-                date = date.replace(hour=0, minute=0, second=0, microsecond=0)
-                daily_stat = RadioListeningStat(date=date, radio=hourly_stat.radio, connections=hourly_stat.connections, audience_peak=hourly_stat.audience_peak, overall_listening_time=hourly_stat.overall_listening_time, favorites=hourly_stat.favorites, likes=hourly_stat.likes, dislikes=hourly_stat.dislikes)
-                results.append(daily_stat)
-            else:
-                audience_peak = daily_stat.audience_peak
-                if hourly_stat.audience_peak > audience_peak:
-                    audience_peak = hourly_stat.audience_peak
-                daily_stat.audience_peak = audience_peak
-                daily_stat.connections += hourly_stat.connections
-                daily_stat.overall_listening_time = hourly_stat.overall_listening_time
-                daily_stat.favorite = hourly_stat.favorites
-                daily_stat.likes = hourly_stat.likes
-                daily_stat.dislikes = hourly_stat.dislikes
-        return results
+        daily_stats = RadioListeningStat.objects.daily_stats(radio, nb_days=30)
+        return daily_stats
 
     def obj_get_list(self, request=None, **kwargs):
         # Filtering disabled for brevity...
--- yaapp/apps/stats/models.py
+++ yaapp/apps/stats/models.py
@@ -1,6 +1,37 @@
 from django.db import models
+from datetime import datetime, timedelta
 
+
+class RadioListeningStatManager(models.Manager):
+    def daily_stats(self, radio, nb_days=30):
+        ref = datetime.now() - timedelta(days=nb_days)
+        original_stats = self.filter(date__gt=ref, radio=radio).order_by('date')
+        
+        results = []
+        # create RadioListeningStat for each day, but DON'T SAVE IT !!! it must not be added in the database
+        date = None
+        daily_stat = None
+        for hourly_stat in original_stats: 
+            if not date or date.date() != hourly_stat.date.date():
+                date = hourly_stat.date
+                date = date.replace(hour=0, minute=0, second=0, microsecond=0)
+                daily_stat = RadioListeningStat(date=date, radio=hourly_stat.radio, connections=hourly_stat.connections, audience_peak=hourly_stat.audience_peak, overall_listening_time=hourly_stat.overall_listening_time, favorites=hourly_stat.favorites, likes=hourly_stat.likes, dislikes=hourly_stat.dislikes)
+                results.append(daily_stat)
+            else:
+                audience_peak = daily_stat.audience_peak
+                if hourly_stat.audience_peak > audience_peak:
+                    audience_peak = hourly_stat.audience_peak
+                daily_stat.audience_peak = audience_peak
+                daily_stat.connections += hourly_stat.connections
+                daily_stat.overall_listening_time = hourly_stat.overall_listening_time
+                daily_stat.favorite = hourly_stat.favorites
+                daily_stat.likes = hourly_stat.likes
+                daily_stat.dislikes = hourly_stat.dislikes
+        return results
+    
+    
 class RadioListeningStat(models.Model):
+    objects = RadioListeningStatManager()
     radio = models.ForeignKey('yabase.Radio')
     date = models.DateTimeField(auto_now_add=True)
     overall_listening_time = models.FloatField()
