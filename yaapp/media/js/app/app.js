a7294b8c1dff9fbb781043ea059284b283e5f84c
Jérôme Blondon <jerome@yasound.com>
2012-04-16 09:49:23 +0200

    webapp wip
    * added missing files
    * added route for radio

--- yaapp/media/js/app/app.js
+++ yaapp/media/js/app/app.js
@@ -1,19 +1,38 @@
 $(document).ready(function() {
     Namespace('Yasound.App');
+    
     Yasound.App.Workspace = Backbone.Router.extend({
         routes : {
             "" : "index",
+            "radio/:id" : "radio",
             "help" : "help", // #help
             "search/:query" : "search", // #search/kiwis
             "search/:query/p:page" : "search" // #search/kiwis/p7
         },
 
+        currentRadio: new Yasound.Data.Models.Radio({id:0}),
+        
+        setCurrentRadioId: function(id) {
+            this.currentRadio.set('id', id);
+            this.currentRadio.fetch();
+        },
+        
         index : function() {
-            var currentRadio = new Yasound.Data.Models.Radio({
-                id : 75
-            });
+            $('#wall').html('Welcome');
+        },
+
+        help : function() {
+            $('#wall').html('Help');
+        },
+
+        search : function(query, page) {
+        },
+        
+        
+        radio: function(id) {
+            this.setCurrentRadioId(id);
             var wallEvents = new Yasound.Data.Models.WallEvents();
-            wallEvents.radio = currentRadio;
+            wallEvents.radio = this.currentRadio;
             var list = new Yasound.Views.WallEvents({
                 collection: wallEvents,
                 el: $('#wall')
@@ -21,16 +40,11 @@ $(document).ready(function() {
             list.addAll();
             
             wallEvents.fetch();
-        },
-
-        help : function() {
-            alert('help');
-        },
-
-        search : function(query, page) {
         }
+        
     });
 
+    
     var router = new Yasound.App.Workspace();
     Backbone.history.start({
         pushState : false,
