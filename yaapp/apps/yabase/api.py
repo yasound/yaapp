85a3403b9f45ee89e8456633389d9860ea9cdb6e
Jérôme Blondon <jerome@yasound.com>
2012-04-17 21:55:38 +0200

    basic skin for wall event messages

--- yaapp/apps/yabase/api.py
+++ yaapp/apps/yabase/api.py
@@ -409,7 +409,7 @@ class WallEventResource(ModelResource):
             return None
         
         WallEvent.objects.add_current_song_event(radio)
-
+        print bundle.data
         wall_event_resource = super(WallEventResource, self).obj_create(bundle, request, **kwargs)
         wall_event_resource.obj.start_date = datetime.datetime.now() # be sure the song event is before message event
         wall_event_resource.obj.save()
