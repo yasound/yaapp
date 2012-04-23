d527c694ba38d1762f47ce068db0eb5916643587
Jérôme Blondon <jerome@yasound.com>
2012-04-23 09:21:35 +0200

    fixed iPad issues

--- yaapp/media/yaweb/js/elecsounds.js
+++ yaapp/media/yaweb/js/elecsounds.js
@@ -1,15 +1,30 @@
 jQuery(document).ready(function() {
+    var isIpad = (navigator.userAgent.match(/iPhone/i)) || (navigator.userAgent.match(/iPod/i)) || (navigator.userAgent.match(/iPad/i));
+
     var f = jQuery("a.boxed").fancybox({
-        'speedIn'       :   200, 
-        'speedOut'      :   200,
-        'width'         :   1517,
-        'height'        :   817,          
-        'overlayShow'   :   true
+        'speedIn' : 200,
+        'speedOut' : 200,
+        'width' : 1517,
+        'height' : 817,
+        'overlayShow' : true,
+        onStart : function() {
+            if (isIpad) {
+                jQuery('object').hide();
+            }
+        },
+        onClosed : function() {
+            if (isIpad) {
+                jQuery('object').show();
+            }
+        }
     });
-    
+
     jQuery("a.boxed").trigger('click');
 
     jQuery(window).resize(function() {
         jQuery.fancybox.resize();
     });
+
+    if ((navigator.userAgent.match(/iPhone/i)) || (navigator.userAgent.match(/iPod/i)) || (navigator.userAgent.match(/iPad/i))) {
+    }
 });
--- yaapp/media/yaweb/js/unlightbox.js
+++ yaapp/media/yaweb/js/unlightbox.js
@@ -1,4 +1,7 @@
 jQuery(document).ready(function() {
+    var isIpad = (navigator.userAgent.match(/iPhone/i)) || (navigator.userAgent.match(/iPod/i)) || (navigator.userAgent.match(/iPad/i));
+
+    
     if (top.location != self.location) {
         jQuery('#unlightbox').show();
     } else {
@@ -6,8 +9,14 @@ jQuery(document).ready(function() {
         jQuery('a#logo').attr('href', '/');
     }
     
+    if (isIpad && top.location != self.location) {
+        top.location = self.location
+    }
+    
     jQuery("#unlightbox a").click(function() {
         top.location = self.location;
     })
+    
+    
     
 });
