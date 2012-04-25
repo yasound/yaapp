c8a71123a06c9c269768e4503a42fea6ed7d2849
Jérôme Blondon <jerome@yasound.com>
2012-04-25 10:01:04 +0200

    keyfigures : added connected users

--- yaapp/apps/yabackoffice/views.py
+++ yaapp/apps/yabackoffice/views.py
@@ -427,6 +427,8 @@ def keyfigures(request, template_name='yabackoffice/keyfigures.html'):
     facebook_user_count = UserProfile.objects.exclude(facebook_token='').count()
     twitter_user_count = UserProfile.objects.exclude(twitter_token='').count()
     yasound_user_count = UserProfile.objects.exclude(yasound_email='').count()
+    connected_user_count = RadioUser.objects.filter(connected=True).count()
+    
     
     return render_to_response(template_name, {
         "user_count": User.objects.filter(is_active=True).count(),
@@ -441,7 +443,8 @@ def keyfigures(request, template_name='yabackoffice/keyfigures.html'):
         "total_friend_count": total_friend_count,
         "facebook_user_count": facebook_user_count,
         "twitter_user_count": twitter_user_count,
-        "yasound_user_count": yasound_user_count
+        "yasound_user_count": yasound_user_count,
+        "connected_user_count": connected_user_count,
     }, context_instance=RequestContext(request))  
 
 
