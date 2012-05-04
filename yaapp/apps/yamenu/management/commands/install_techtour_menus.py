d449441c720c93404c4cb0e75d00fb883daaa0f9
mat <campion.matthieu@gmail.com>
2012-05-04 15:51:30 +0200

    fixed bug with ios token saving + added command to install tech tour menus

---
+++ yaapp/apps/yamenu/management/commands/install_techtour_menus.py
@@ -0,0 +1,15 @@
+from django.core.management.base import BaseCommand
+from yamenu.models import MenusManager
+import logging
+logger = logging.getLogger("yaapp.yamenu")
+
+class Command(BaseCommand):
+    """
+    Update action states
+    """
+    help = "Install tech tour menu descriptions"
+    args = ''
+
+    def handle(self, *app_labels, **options):
+        mm = MenusManager()
+        mm.install_techtour()
