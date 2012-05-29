5d2345203d7a8816ce4a841f706531352d59c757
Jérôme Blondon <jerome@yasound.com>
2012-05-29 16:16:36 +0200

    facebook open graph wip
    * added like & listen actions

--- yaapp/apps/yagraph/models.py
+++ yaapp/apps/yagraph/models.py
@@ -1,6 +1,6 @@
 from django.conf import settings
 from yabase import settings as yabase_settings, signals as yabase_signals
-from yagraph.task import async_post_message
+from yagraph.task import async_post_message, async_listen, async_like_song
 
 def new_wall_event_handler(sender, wall_event, **kwargs):
     user = wall_event.user
@@ -12,9 +12,25 @@ def new_wall_event_handler(sender, wall_event, **kwargs):
     we_type = wall_event.type
     if we_type == yabase_settings.EVENT_MESSAGE:
         async_post_message.delay(user.id, wall_event.radio.uuid, wall_event.text)
+    elif we_type == yabase_settings.EVENT_LIKE:
+        song_title = unicode(wall_event)
+        async_like_song.delay(user.id, wall_event.radio.uuid, song_title)
+        
+def user_started_listening_handler(sender, radio, user):
+    if user is None:
+        return
+    if user.is_anonymous():
+        return
+    
+    current_song = radio.current_song
+    if current_song is None:
+        return
+    
+    song_title = unicode(current_song)
+    async_listen.delay(user.id, radio.uuid, song_title)
 
 def install_handlers():
     yabase_signals.new_wall_event.connect(new_wall_event_handler)
-
+    yabase_signals.user_started_listening.connect(user_started_listening_handler)
 if settings.FACEBOOK_OPEN_GRAPH_ENABLED:
     install_handlers()    
--- yaapp/apps/yagraph/task.py
+++ yaapp/apps/yagraph/task.py
@@ -5,24 +5,60 @@ from facepy import GraphAPI
 from yacore.http import absolute_url
 from django.conf import settings
 
-
-@task(ignore_result=True)
-def async_post_message(user_id, radio_uuid, message):
+def _facebook_token(user_id):
     try:
         user_profile = UserProfile.objects.get(user__id=user_id)
     except:
-        return
+        return None
     
     if not user_profile.facebook_enabled or len(user_profile.facebook_token) <= 0:
+        return None
+    facebook_token = user_profile.facebook_token
+    return facebook_token
+
+@task(ignore_result=True)
+def async_post_message(user_id, radio_uuid, message):
+    facebook_token = _facebook_token(user_id)
+    if facebook_token is None:
         return
     
     radio_url = absolute_url(reverse('webapp_radio', args=[radio_uuid])) 
-    
-    facebook_token = user_profile.facebook_token
-    graph = GraphAPI(facebook_token)
-    
     data = {
        'radio':radio_url
     }
-    path = 'me/%s:post_a_message' % (settings.FACEBOOK_APP_NAMESPACE)
+    path = 'me/%s:post_message' % (settings.FACEBOOK_APP_NAMESPACE)
+
+    graph = GraphAPI(facebook_token)
+    graph.post(path=path, data=data)
+    
+@task(ignore_result=True)
+def async_listen(user_id, radio_uuid, song_title):
+    facebook_token = _facebook_token(user_id)
+    if facebook_token is None:
+        return
+
+    radio_url = absolute_url(reverse('webapp_radio', args=[radio_uuid])) 
+    data = {
+       'radio':radio_url,
+       'song': song_title
+    }
+    path = 'me/%s:listen' % (settings.FACEBOOK_APP_NAMESPACE)
+
+    graph = GraphAPI(facebook_token)
+    graph.post(path=path, data=data)
+
+@task(ignore_result=True)
+def async_like_song(user_id, radio_uuid, song_title):
+    facebook_token = _facebook_token(user_id)
+    if facebook_token is None:
+        return
+
+    radio_url = absolute_url(reverse('webapp_radio', args=[radio_uuid])) 
+    data = {
+       'radio':radio_url,
+       'song': song_title
+    }
+    path = 'me/%s:like' % (settings.FACEBOOK_APP_NAMESPACE)
+
+    graph = GraphAPI(facebook_token)
     graph.post(path=path, data=data)
