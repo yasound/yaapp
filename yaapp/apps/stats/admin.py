a75cfb2477e3b40134645d8a836d025f03e743a0
Jérôme Blondon <jerome@yasound.com>
2012-05-30 15:53:25 +0200

    fixed search error in admin page for listening stats

--- yaapp/apps/stats/admin.py
+++ yaapp/apps/stats/admin.py
@@ -3,6 +3,6 @@ from models import RadioListeningStat
 
 class RadioListeningStatAdmin(admin.ModelAdmin):
     list_display = ('radio', 'overall_listening_time', 'audience_peak', 'connections', 'favorites', 'likes', 'dislikes', 'date')
-    search_fields = ( 'radio', )
+    search_fields = ( 'radio__name', )
     list_filter = ('radio', 'date')
 admin.site.register(RadioListeningStat, RadioListeningStatAdmin)
