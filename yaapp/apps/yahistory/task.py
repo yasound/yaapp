00b854a39e3bd39161b45cebd9adb4371bcd5fb0
Jérôme Blondon <jerome@yasound.com>
2012-06-06 07:41:13 +0200

    yahistory: async_add_like_song_event() radio_uuid parameter is useless

--- yaapp/apps/yahistory/task.py
+++ yaapp/apps/yahistory/task.py
@@ -14,7 +14,7 @@ def async_add_post_message_event(user_id, radio_uuid, message):
     uh.add_post_message_event(user_id, radio_uuid, message)
 
 @task(ignore_result=True)
-def async_add_like_song_event(user_id, radio_uuid, song_id):
+def async_add_like_song_event(user_id, song_id):
     from yahistory.models import UserHistory
     uh = UserHistory()
     uh.add_like_song_event(user_id, song_id)
