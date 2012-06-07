cd024f1dd443474ffb0524669ec90ee63acbe8a0
mat <campion.matthieu@gmail.com>
2012-06-07 15:30:18 +0200

    sacem reporting in one file (for all radios)

--- yaapp/apps/yareport/settings.py
+++ yaapp/apps/yareport/settings.py
@@ -1,4 +1,4 @@
-sacem_declarant_identifier_short = '123'
-sacem_declarant_identifier_full = 'yasound_sacem_ID'
+sacem_declarant_identifier_short = 'YAS'
+sacem_declarant_identifier_full = 'yasound'
 sacem_declarant_address_line1 = '144/146 rue des poissonniers'
 sacem_declarant_address_line2 = '75018 Paris'
