ed3707b9f544b39810caa379df2fbe3f4b20d1cf
Jérôme Blondon <jerome@yasound.com>
2012-06-21 14:10:26 +0200

    recommendation: wip
    * fixed classification manager
    * added tests

--- yaapp/apps/yarecommendation/models.py
+++ yaapp/apps/yarecommendation/models.py
@@ -1,26 +1,40 @@
 from django.conf import settings
 from pymongo import DESCENDING
-from yabase.models import SongMetadata
-from yaref.models import YasoundSong, YasoundGenre, YasoundSongGenre
+from yabase.models import SongMetadata, Radio
+from yaref.models import YasoundSong, YasoundGenre
 
 
 class ClassifiedRadiosManager():
-    
     def __init__(self):
         self.db = settings.MONGO_DB
-        self.collection = self.db.history.users
+        self.collection = self.db.recommendation.radios
         self.collection.ensure_index("db_id")
     
+    def drop(self):
+        self.collection.drop()
     
     def add_radio(self, radio):
-        songs = YasoundSong.objects.filter(id__in=SongMetadata.objects.filter(songinstance__playlist__radio=radio))
+        ids = list(SongMetadata.objects.filter(songinstance__playlist__radio=radio).values_list('yasound_song_id', flat=True))
+        songs = YasoundSong.objects.filter(id__in=ids)
+        
+        classification = {}
         for song in songs:
-            classification = {}
             genres = YasoundGenre.objects.filter(yasoundsonggenre__song=song)
             for genre in genres:
-                classification[genre] = classification.get(genre, 0) +1 
-                
-        doc = {
-            'db_id' : song.id,
-            'classification': classification
-        }
+                classification[genre.name_canonical] = classification.get(genre.name_canonical, 0) +1 
+        
+        if len(classification.keys()) > 0:
+            doc = {
+                'db_id' : radio.id,
+                'classification': classification
+            }
+            self.collection.update({'db_id': radio.id}, {'$set': doc}, upsert=True, safe=True)
+    
+    def populate(self):
+        radios = Radio.objects.filter(ready=True)
+        self.drop()
+        for radio in radios:
+            self.add_radio(radio)
+            
+    def all(self):
+        return self.collection.find()
---
+++ yaapp/apps/yarecommendation/tests.py
@@ -0,0 +1,50 @@
+# -*- coding: utf-8 -*-
+from django.contrib.auth.models import User
+from django.test import TestCase
+from models import ClassifiedRadiosManager
+from yabase import tests_utils as yabase_test_utils
+from yabase.models import Radio
+from yaref import test_utils as yaref_test_utils
+from yaref.models import YasoundGenre, YasoundSongGenre, YasoundSong
+
+class TestClassification(TestCase):
+    def setUp(self):
+        cm = ClassifiedRadiosManager()
+        cm.drop()
+        user = User(email="test@yasound.com", username="test", is_superuser=False, is_staff=False)
+        user.set_password('test')
+        user.save()
+        self.client.login(username="test", password="test")
+        self.user = user        
+
+
+    def testManager(self):
+        for i in range(0, 10):
+            YasoundGenre.objects.create(name='genre_%d' % (i), name_canonical = 'genre_%d' % (i))
+        
+        for i in range(0, 10):
+            song = yaref_test_utils.generate_yasound_song(name='name %d' % (i), album='album', artist='artist')
+            YasoundSongGenre.objects.create(song=song, genre=YasoundGenre.objects.get(id=i+1))
+
+        radio = Radio.objects.radio_for_user(self.user)
+        playlist = yabase_test_utils.generate_playlist(song_count=10)
+        playlist.radio = radio
+        playlist.save()
+        radio.ready=True
+        radio.save()
+        
+        cm = ClassifiedRadiosManager()
+        cm.add_radio(radio)
+        
+        doc = cm.all()[0]
+        classification = doc.get('classification')
+        self.assertEquals(classification.get('genre_0'), 1)
+
+        song = YasoundSong.objects.get(id=2)        
+        YasoundSongGenre.objects.create(song=song, genre=YasoundGenre.objects.get(id=1))
+        cm.add_radio(radio)
+        
+        doc = cm.all()[0]
+        classification = doc.get('classification')
+        self.assertEquals(classification.get('genre_0'), 2)
+        
