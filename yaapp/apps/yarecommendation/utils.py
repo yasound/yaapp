99aeb992ea33d56efb81c9161940705deff9fc2d
Jérôme Blondon <jerome@yasound.com>
2012-06-22 10:32:16 +0200

    yarecommendation: added pearson distance

--- yaapp/apps/yarecommendation/utils.py
+++ yaapp/apps/yarecommendation/utils.py
@@ -1,3 +1,5 @@
+from math import sqrt
+
 def sim_distance(doc1, doc2):
     # Get the list of shared_items
     si = {}
@@ -11,6 +13,31 @@ def sim_distance(doc1, doc2):
                         for item in doc1['classification'] if item in doc2['classification']])
     return 1 / (1 + sum_of_squares)
 
+# Returns the Pearson correlation coefficient for p1 and p2
+def sim_pearson(doc1, doc2):
+    # Get the list of mutually rated items
+    si = {}
+    for item in doc1.get('classification').keys():
+        if item in doc2.get('classification').keys(): si[item] = 1
+    # Find the number of elements
+    n = len(si)
+    # if they are no ratings in common, return 0
+    if n == 0: return 0
+    # Add up all the preferences
+    sum1 = sum([doc1.get('classification')[it] for it in si])
+    sum2 = sum([doc2.get('classification')[it] for it in si])
+    # Sum up the squares
+    sum1Sq = sum([pow(doc1.get('classification')[it], 2) for it in si])
+    sum2Sq = sum([pow(doc2.get('classification')[it], 2) for it in si])
+    # Sum up the products
+    pSum = sum([doc1.get('classification')[it] * doc2.get('classification')[it] for it in si])
+    # Calculate Pearson score
+    num = pSum - (sum1 * sum2 / n)
+    den = sqrt((sum1Sq - pow(sum1, 2) / n) * (sum2Sq - pow(sum2, 2) / n))
+    if den == 0: return 0
+    r = num / den
+    return r
+
 # Returns the best matches for person from the prefs dictionary.
 # Number of results and similarity function are optional params.
 def top_matches(docs, doc, n=5, similarity=sim_distance):
@@ -19,5 +46,5 @@ def top_matches(docs, doc, n=5, similarity=sim_distance):
     # Sort the list so the highest scores appear at the top scores.sort( )
     scores.sort()
     scores.reverse()
-    limit =  scores[0:n]
+    limit = scores[0:n]
     return [db_id for (_score, db_id) in limit]
