cf8805f6e77dfaa604bc2965cb19ee64984634d8
Jérôme Blondon <jerome@yasound.com>
2012-06-26 14:48:14 +0200

    get_recommendation helper commands takes radio_id as argument

--- yaapp/apps/yarecommendation/management/commands/get_recommendation.py
+++ yaapp/apps/yarecommendation/management/commands/get_recommendation.py
@@ -13,13 +13,29 @@ class Command(BaseCommand):
     Check state of songs on filesystem
     """
     option_list = BaseCommand.option_list + (
+        make_option('-r', '--radio', dest='radio_id',
+            default=0, help="radio id"),
     )
     help = "Build radio recommendation data"
     args = ''
 
     def handle(self, *app_labels, **options):
+        radio_id = int(options.get('radio_id', 0))
         logger.info("getting recommendation")
         cm = ClassifiedRadiosManager()
-        reco = cm.find_similar_radios(['air', 'benjamin biolay', 'the cure'])
-        logger.info(reco)
+        
+        doc = cm.radio_doc(radio_id)
+        if doc is None:
+            logger.info('radio not found, exiting')
+            return
+        
+        artists = doc.get('artists')
+        
+        reco = cm.find_similar_radios(artists)
+        result = []
+        for rec in reco:
+            if rec[1] == radio_id:
+                continue
+            result.append(rec)
+        logger.info(result)
         logger.info("done")
