a7416be81f4b98c3c43cb78f53f65644414ae516
Jérôme Blondon <jerome@yasound.com>
2012-07-19 10:15:00 +0200

    added missing file

---
+++ yaapp/media/js/app/templateLoader.js
@@ -0,0 +1,10 @@
+window.ich.loadRemoteTemplate = function (path, name, callback) {
+    if (!ich.templates[name]) {
+        jQuery.get("/app/tpl/" + path, function (data) {
+            window.ich.addTemplate(name, data);
+            callback();
+        });
+    } else {
+        callback();
+    }
+};
