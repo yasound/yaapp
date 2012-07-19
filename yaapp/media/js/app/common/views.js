f5eb6ac794a1a88879310fa0e2662e907a2c7a92
Jérôme Blondon <jerome@yasound.com>
2012-07-19 08:43:59 +0200

    fixed boulette

--- yaapp/media/js/app/common/views.js
+++ yaapp/media/js/app/common/views.js
@@ -478,7 +478,7 @@ Yasound.Views.Pagination = Backbone.View.extend({
         var height = $(document).height();
         var scrollHeight = $(window).scrollTop() + $(window).height();
 
-        if (scrollHeight >= 100) {
+        if (scrollHeight >= height) {
             this.locked = true;
             this.collection.requestNextPage();
         }
