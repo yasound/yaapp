6e29dd2770008f213cd843c95a062cfb8d339f99
Jérôme Blondon <jerome@yasound.com>
2012-08-16 17:02:42 +0200

    updated settings css

--- yaapp/apps/yabase/forms.py
+++ yaapp/apps/yabase/forms.py
@@ -25,6 +25,7 @@ class SettingsRadioForm(BootstrapModelForm):
 
 
 class MyInformationsForm(BootstrapModelForm):
+    bio_text = forms.CharField(label=_('Biography'), widget=forms.widgets.Textarea(attrs={'rows':5, 'cols':60}))
     class Meta:
         model = UserProfile
         fields = ('name', 'url', 'bio_text', 'picture', 'birthday', 'gender', 'city')
