29ca5fee9779d08d7c3a7523f202633f12f8ea6c
Jérôme Blondon <jerome@yasound.com>
2012-08-16 13:23:54 +0200

    template loader: added trailing slash
    without trainling slash, a 301 redirect is issued but without https as protocol which leads to xhr loading error

--- yaapp/media/js/app/templateLoader.js
+++ yaapp/media/js/app/templateLoader.js
@@ -1,6 +1,6 @@
 window.ich.loadRemoteTemplate = function (path, name, callback) {
     if (!ich.templates[name]) {
-        jQuery.get("/app/tpl/" + path, function (data) {
+        jQuery.get("/app/tpl/" + path + "/", function (data) {
             window.ich.addTemplate(name, data);
             callback();
         });
