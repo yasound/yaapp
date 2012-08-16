83c696ecd9a44d1b2f7b77f0df01de1dfe8ee2a7
Jérôme Blondon <jerome@yasound.com>
2012-08-16 15:05:08 +0200

    fixed re-edition problem of radio

--- yaapp/media/js/app/templateLoader.js
+++ yaapp/media/js/app/templateLoader.js
@@ -1,6 +1,7 @@
 window.ich.loadRemoteTemplate = function (path, name, callback, data) {
     if (data) {
         delete window.ich[name];
+        delete window.ich.templates[name];
         jQuery.get("/app/tpl/" + path + "/", data, function (data) {
             window.ich.addTemplate(name, data); 
             callback();
