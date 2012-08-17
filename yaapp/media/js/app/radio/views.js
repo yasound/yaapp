2b3ca94d20cdf1f7c74aecf5f971794b8430e33e
ferdinand <doumic.ferdinand@gmail.com>
2012-08-17 16:45:14 +0200

    skin radio edit combobox

--- yaapp/media/js/app/radio/views.js
+++ yaapp/media/js/app/radio/views.js
@@ -547,6 +547,7 @@ Yasound.Views.EditRadioPage = Backbone.View.extend({
 
     templateLoaded: function() {
         $(this.el).html(ich.editRadioPageTemplate());
+        $("select").uniform();
         var that = this;
         var $progress = $('#progress .bar', this.el);
         $progress.parent().hide();
