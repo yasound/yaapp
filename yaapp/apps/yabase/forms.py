89f8d962009745ffbba60aa98ed4d75f6fe5cb68
Jérôme Blondon <jerome@yasound.com>
2012-08-22 10:37:54 +0200

    * fixed settings access when anonymous * fixed password reset if field is empty

--- yaapp/apps/yabase/forms.py
+++ yaapp/apps/yabase/forms.py
@@ -83,8 +83,10 @@ class MyAccountsForm(BootstrapModelForm):
 
     def save(self):
         if "password1" in self.cleaned_data:
-            self.instance.user.set_password(self.cleaned_data['password1'])
-            self.instance.user.save()
+            password = self.cleaned_data['password1']
+            if len(password) > 0:
+                self.instance.user.set_password(self.cleaned_data['password1'])
+                self.instance.user.save()
 
 class MyNotificationsForm(BootstrapForm):
     fb_share_listen = forms.BooleanField(label=_("Listen"), required=False)
