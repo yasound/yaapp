4ad8967798cab31798e0c136c993a2aafaba8474
Jérôme Blondon <jerome@yasound.com>
2012-08-24 14:51:06 +0200

    updated reencode song command

--- yaapp/apps/yaref/utils.py
+++ yaapp/apps/yaref/utils.py
@@ -1,3 +1,4 @@
+import subprocess as sub
 
 def convert_filename_to_filepath(filename):
     """
@@ -10,3 +11,15 @@ def convert_filename_to_filepath(filename):
     part3 = filename[6:9]
     extension = filename[-3:]
     return '%s/%s/%s.%s' % (part1, part2, part3, extension)
+
+def convert_to_mp3(ffmpeg_bin, ffmpeg_options, source, destination):
+    args = [ffmpeg_bin,
+            '-i',
+            source]
+    args.extend(ffmpeg_options.split(" "))
+    args.append(destination)
+    p = sub.Popen(args,stdout=sub.PIPE,stderr=sub.PIPE)
+    output, errors = p.communicate()
+    if len(errors) == 0:
+        return False
+    return True
