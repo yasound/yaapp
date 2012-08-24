44f44caf118055b8d2787f94efd8b2d12a5a4f34
Jérôme Blondon <jerome@yasound.com>
2012-08-24 17:10:00 +0200

    wip on subscriptions

--- yaapp/apps/yapremium/urls.py
+++ yaapp/apps/yapremium/urls.py
@@ -2,4 +2,5 @@ from django.conf.urls.defaults import *
 
 urlpatterns = patterns('yapremium.views',
     url(r'^subscriptions/$', 'subscriptions'),
+    url(r'^subscriptions/(?P<subscription_id>\d+)$', 'subscriptions'),
 )
