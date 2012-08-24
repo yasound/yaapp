b122c3144b3ad908545b5c347fdb9e3592ff72b9
Jérôme Blondon <jerome@yasound.com>
2012-08-24 17:10:08 +0200

    wip on subscriptions

--- yaapp/apps/yapremium/views.py
+++ yaapp/apps/yapremium/views.py
@@ -4,16 +4,19 @@ from yacore.decorators import check_api_key
 import logging
 logger = logging.getLogger("yaapp.yapremium")
 
-@check_api_key(methods=['GET',], login_required=False)
-def subscriptions(request):
-    limit = int(request.REQUEST.get('limit', 25))
-    offset = int(request.REQUEST.get('offset', 0))
-    qs = Subscription.objects.available_subscriptions()
-    total_count = qs.count()
-    qs = qs[offset:offset+limit]
-    data = []
-    for subscription in qs:
-        data.append(subscription.as_dict())
-    response = api_response(data, total_count, limit=limit, offset=offset)
-    return response
-
+@check_api_key(methods=['GET',], login_required=True)
+def subscriptions(request, subscription_id=None):
+    if request.method == 'GET':
+        limit = int(request.REQUEST.get('limit', 25))
+        offset = int(request.REQUEST.get('offset', 0))
+        qs = Subscription.objects.available_subscriptions()
+        total_count = qs.count()
+        qs = qs[offset:offset+limit]
+        data = []
+        for subscription in qs:
+            data.append(subscription.as_dict())
+        response = api_response(data, total_count, limit=limit, offset=offset)
+        return response
+    elif request.method == 'POST' and subscription_id is not None:
+        data = json.loads(request.raw_post_data)
+    raise Http404
