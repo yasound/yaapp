e5eea278efb5a6ac23506312bc83a7034375a270
Jérôme Blondon <jerome@yasound.com>
2012-08-27 11:28:55 +0200

    added logs when checking apple receipt

--- yaapp/apps/yapremium/utils.py
+++ yaapp/apps/yapremium/utils.py
@@ -3,18 +3,21 @@ import json
 import base64
 import urllib2
 
+import logging
+logger = logging.getLogger("yaapp.yapremium")
+
 def verifiy_receipt(receipt):
     """
     verify receipt on apple server
     """
     receipt_data = base64.b64encode(receipt)
     json_data = json.dumps({'receipt-data': receipt_data})
+    r = requests.post(settings.APPLE_VERIFY_RECEIPT_URL, data=json_data)
+    logger.debug(r.raw)
+    logger.debug(r.status_code)
+    response = r.json
+    logger.debug(response)
+    if r.status_code == 200 and response.get('status') == 0:
+        return True
 
-    try:
-        r = requests.post(settings.APPLE_VERIFY_RECEIPT_URL, data=json_data)
-        response = r.json
-        if r.status_code == 200 and response.get('status') == 0:
-            return True
-    except:
-        return False
     return False
