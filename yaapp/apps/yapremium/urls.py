cb2fc60eb57a2f5b93a81f377f052fed89755571
Jérôme Blondon <jerome@yasound.com>
2012-08-27 09:52:27 +0200

    fixed url regexp for yapremium POST sub

--- yaapp/apps/yapremium/urls.py
+++ yaapp/apps/yapremium/urls.py
@@ -2,5 +2,5 @@ from django.conf.urls.defaults import *
 
 urlpatterns = patterns('yapremium.views',
     url(r'^subscriptions/$', 'subscriptions'),
-    url(r'^subscriptions/(?P<subscription_sku>\S+)$', 'subscriptions'),
+    url(r'^subscriptions/(?P<subscription_sku>\S+)/$', 'subscriptions'),
 )
