f33a919c1cf14a8659e20c369916d338797e5aff
Jérôme Blondon <jerome@yasound.com>
2012-09-07 14:03:37 +0200

    added twitter  & facebook gift handler
    * added new signals in account when adding/removing twitter/facebook/yasound accounts
    * added handlers in yapremium

--- yaapp/apps/yapremium/settings.py
+++ yaapp/apps/yapremium/settings.py
@@ -2,10 +2,14 @@ from django.utils.translation import ugettext_lazy as _
 
 ACTION_CREATE_ACCOUNT = 0
 ACTION_WATCH_TUTORIAL = 1
+ACTION_ADD_FACEBOOK_ACCOUNT = 2
+ACTION_ADD_TWITTER_ACCOUNT = 3
 
 ACTION_CHOICES = (
     (ACTION_CREATE_ACCOUNT, _('Create account')),
     (ACTION_WATCH_TUTORIAL, _('Watch tutorial')),
+    (ACTION_ADD_FACEBOOK_ACCOUNT, _('Add facebook account')),
+    (ACTION_ADD_TWITTER_ACCOUNT, _('Add twitter account')),
 )
 
 
