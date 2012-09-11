413e4437323e01027db29dc3a75c867c1415a3dd
Jérôme Blondon <jerome@yasound.com>
2012-09-11 09:52:05 +0200

    fixed signal name

--- yaapp/apps/account/signals.py
+++ yaapp/apps/account/signals.py
@@ -2,7 +2,7 @@ import django.dispatch
 
 new_device_registered = django.dispatch.Signal(providing_args=['user', 'uuid', 'ios_token'])
 facebook_account_added = django.dispatch.Signal(providing_args=['user'])
-facebook_account_remove = django.dispatch.Signal(providing_args=['user'])
+facebook_account_removed = django.dispatch.Signal(providing_args=['user'])
 twitter_account_added = django.dispatch.Signal(providing_args=['user'])
 twitter_account_removed = django.dispatch.Signal(providing_args=['user'])
 yasound_account_added = django.dispatch.Signal(providing_args=['user'])
