d825d86fa223fee0d18ecb2cf3927639f096142e
Jérôme Blondon <jerome@yasound.com>
2012-10-03 11:27:59 +0200

    added badge on top radios

--- yaapp/media/js/app/top/views.js
+++ yaapp/media/js/app/top/views.js
@@ -2,6 +2,90 @@
 /*extern Ext, $ */
 Namespace('Yasound.Views');
 
+Yasound.Views.TopResults = Backbone.View.extend({
+    initialize: function() {
+        _.bindAll(this, 'addOne', 'addAll');
+
+        this.collection.bind('add', this.addOne, this);
+        this.collection.bind('reset', this.addAll, this);
+        this.collection.bind('beforeFetch', this.beforeFetch, this);
+        this.views = [];
+        this.index = 1;
+    },
+    onClose: function() {
+        this.collection.unbind('beforeFetch', this.beforeFetch);
+        this.collection.unbind('add', this.addOne);
+        this.collection.unbind('reset', this.addAll);
+    },
+
+    beforeFetch: function() {
+        if (this.loadingMask) {
+            $(this.el).append(this.loadingMask);
+        }
+    },
+    addAll: function() {
+        var mask = $('.loading-mask', this.el);
+        if (!this.loadingMask) {
+            this.loadingMask = mask;
+        }
+        mask.hide();
+
+        if (this.collection.length == 0) {
+            $('.empty', this.el).show();
+        } else {
+            $('.empty', this.el).hide();
+        }
+        this.collection.each(this.addOne);
+    },
+
+    clear: function() {
+        _.map(this.views, function(view) {
+            view.close();
+        });
+        this.views = [];
+        ths.index = 1;
+    },
+
+    addOne: function(radio) {
+        var currentId = radio.id;
+
+        var found = _.find(this.views, function(view) {
+            if (view.model.id == radio.id) {
+                return true;
+            }
+        });
+        if (found) {
+            // do not insert duplicated content
+            return;
+        }
+
+        var view = new Yasound.Views.RadioTopCell({
+            model: radio
+        });
+        $(this.el).append(view.render(this.index++).el);
+        this.views.push(view);
+    }
+});
+
+Yasound.Views.RadioTopCell = Yasound.Views.RadioCell.extend({
+    render: function (index) {
+        var data = this.model.toJSON();
+        data['top_rank'] = index;
+        console.log(data['top_rank'])
+        if (data.name.length > 18) {
+            data.name = data.name.substring(0,18) + "...";
+        }
+        if (Yasound.App.enableFX) {
+            $(this.el).hide().html(ich.radioCellTemplate(data)).fadeIn(200);
+        } else {
+            $(this.el).html(ich.radioCellTemplate(data));
+        }
+
+        this.currentSongModel.set('radioId', this.model.get('id'));
+        return this;
+    }
+});
+
 Yasound.Views.TopRadiosPage = Backbone.View.extend({
     collection: new Yasound.Data.Models.MostActiveRadios({}),
 
@@ -27,7 +111,7 @@ Yasound.Views.TopRadiosPage = Backbone.View.extend({
         $(this.el).html(ich.topRadiosPageTemplate());
         this.collection.perPage = Yasound.Utils.cellsPerPage();
 
-        this.resultsView = new Yasound.Views.SearchResults({
+        this.resultsView = new Yasound.Views.TopResults({
             collection: this.collection,
             el: $('#results', this.el)
         });
