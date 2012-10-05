3e47ec784f86e87cb8ef7780ed6ad3db5fe6bcca
Jérôme Blondon <jerome@yasound.com>
2012-10-05 13:48:08 +0200

    redirect to webapp after successful email verification

--- yaapp/apps/emailconfirmation/views.py
+++ yaapp/apps/emailconfirmation/views.py
@@ -1,10 +1,11 @@
-from django.http import HttpResponseRedirect,Http404
+from django.contrib.auth import login, authenticate
+from django.contrib.auth.models import User
+from django.core.urlresolvers import reverse
+from django.http import HttpResponseRedirect, Http404
 from django.shortcuts import render_to_response
 from django.template import RequestContext
-from django.contrib.auth import login, authenticate
-
-from django.contrib.auth.models import User
 from emailconfirmation.models import EmailConfirmation
+
 
 
 def confirm_email(request, confirmation_key):
@@ -17,7 +18,7 @@ def confirm_email(request, confirmation_key):
         user.save()
         email_address.user.backend='django.contrib.auth.backends.ModelBackend'
         login(request, email_address.user)
-
+        return HttpResponseRedirect(reverse('webapp_default'))
     return render_to_response("emailconfirmation/confirm_email.html", {
         "email_address": email_address,
     }, context_instance=RequestContext(request))
