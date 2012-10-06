9535560e52fcb17fd671952b50015362d89ca881
Jérôme Blondon <jerome@yasound.com>
2012-10-06 22:15:19 +0200

    hack for k-pop radio (number of favs is static)

--- yaapp/apps/yabase/models.py
+++ yaapp/apps/yabase/models.py
@@ -1355,6 +1355,10 @@ class RadioUser(models.Model):
         if not same_favorite:
             radio = self.radio
             radio.favorites = RadioUser.objects.filter(radio=radio, favorite=True).count()
+
+            # JBL : remove when operation is finished
+            if radio.id == 10853:
+                radio.favorites = 121
             radio.save()
             if self.favorite:
                 radio.added_in_favorites(self.user)
