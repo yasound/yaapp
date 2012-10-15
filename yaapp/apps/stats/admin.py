f935bff32ff466f2f966001ad7188cfce91ae60f
Jérôme Blondon <jerome@yasound.com>
2012-10-15 10:32:40 +0200

    updated admin page of radio stats: removed group by and added radio as raw field (improve perfs)

--- yaapp/apps/stats/admin.py
+++ yaapp/apps/stats/admin.py
@@ -4,5 +4,5 @@ from models import RadioListeningStat
 class RadioListeningStatAdmin(admin.ModelAdmin):
     list_display = ('radio', 'overall_listening_time', 'audience_peak', 'connections', 'favorites', 'likes', 'dislikes', 'date')
     search_fields = ( 'radio__name', )
-    list_filter = ('radio', 'date')
+    raw_id_fields = ('radio',)
 admin.site.register(RadioListeningStat, RadioListeningStatAdmin)
