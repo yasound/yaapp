0be4bb886cf144270c53e9a11212d63f9665047a
Jérôme Blondon <jerome@yasound.com>
2012-10-19 08:32:01 +0200

    updated install scripts

--- docs/sysadmin/install.sh
+++ docs/sysadmin/install.sh
@@ -1,5 +1,16 @@
 #!/bin/sh
 
+# create /data/ folders
+mkdir /data/logs
+mkdir /data/tmp
+mkdir /data/var
+mkdir /data/var/lib
+mkdir /data/var/lib/celery
+
+# change rights to customer
+chown customer:customer /data/logs
+chown customer:customer /data/tmp
+chown customer:customer /data/var/lib/celery
 
 # copy files
 cp ./etc/init.d/* /etc/init.d
@@ -7,10 +18,10 @@ cp ./etc/default/* /etc/default/
 cp -r ./etc/gunicorn /etc/
 cp ./etc/nginx/sites-available/yaapp /etc/nginx/sites-available/
 
-# ffmpeg
+# ffmpeg static binary
 cp ./bin/ffmpeg /usr/local/bin/
 
-# nginx
+# nginx configuration
 cd /etc/nginx/sites-enabled
 ln -s ../sites-available/yaapp yaapp
 
