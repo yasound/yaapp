0a6f824f19ac1eb6ab8ca1b84a7279f1cca5af89
Jérôme Blondon <jerome@yasound.com>
2012-10-31 13:48:56 +0100

    yacore is now a django app (which allow translation for instance), added send_mail function in yacore (which contains promotional footer and html content)

---
+++ yaapp/apps/yacore/mail.py
@@ -0,0 +1,17 @@
+from django.core.mail import EmailMultiAlternatives
+from django.utils.translation import ugettext as _
+
+
+def send_mail(from_email, to, subject, content, content_html=None, include_yasound_footer=True):
+    """Convenient function to send email in text & html and with additional footer promoting Yasound
+    """
+
+    if content_html is None:
+        content_html = content
+
+    if include_yasound_footer:
+        content = content + '\n\n' + _("With Yasound (https://yasound.com), there's no need to listen to music alone anymore. Create your own radio station instantly and share with your friends in real time online and on your phone.")
+        content_html = content_html + '<br/><br/>' + _("With <a href='https://yasound.com'>Yasound</a>, there's no need to listen to music alone anymore. Create your own radio station instantly and share with your friends in real time online and on your phone.")
+    msg = EmailMultiAlternatives(subject, content, from_email, [to])
+    msg.attach_alternative(content_html, "text/html")
+    msg.send()
