4068f51cdc465284c3badb22044c1fd29cff9274
Jérôme Blondon <jerome@yasound.com>
2012-10-31 13:56:00 +0100

    remove html tags from user content email

--- yaapp/apps/yacore/mail.py
+++ yaapp/apps/yacore/mail.py
@@ -1,10 +1,16 @@
 from django.core.mail import EmailMultiAlternatives
 from django.utils.translation import ugettext as _
+from django.template.defaultfilters import striptags
 
 
 def send_mail(from_email, to, subject, content, content_html=None, include_yasound_footer=True):
     """Convenient function to send email in text & html and with additional footer promoting Yasound
     """
+
+    if content == '':
+        return
+
+    content = striptags(content)
 
     if content_html is None:
         content_html = content
