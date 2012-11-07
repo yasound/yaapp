bcb9e81d5ec85613a1ff5cc58fb15d4e74366854
Jérôme Blondon <jerome@yasound.com>
2012-11-07 11:03:25 +0100

    added debugtoolbar : this will help to find out slow queries

--- yaapp/settings.py
+++ yaapp/settings.py
@@ -306,6 +306,7 @@ TEMPLATE_LOADERS = (
 
 MIDDLEWARE_CLASSES = (
     'django.middleware.gzip.GZipMiddleware',
+#    'debug_toolbar.middleware.DebugToolbarMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -397,6 +398,7 @@ INSTALLED_APPS = (
     'yashow',
     'compressor',
     'radioways',
+    'debug_toolbar',
 )
 
 MESSAGE_STORAGE = 'django.contrib.messages.storage.session.SessionStorage'
@@ -988,3 +990,17 @@ YASOUND_TWITTER_ACCOUNTS = {
     'FR': 'YasoundSAS'
 }
 YASOUND_TWITTER_DEFAULT_ACCOUNT = 'YasoundUS'
+
+# django debug toolbar
+INTERNAL_IPS = ('127.0.0.1',)
+DEBUG_TOOLBAR_PANELS = (
+    'debug_toolbar.panels.version.VersionDebugPanel',
+    'debug_toolbar.panels.timer.TimerDebugPanel',
+    'debug_toolbar.panels.settings_vars.SettingsVarsDebugPanel',
+    'debug_toolbar.panels.headers.HeaderDebugPanel',
+    'debug_toolbar.panels.request_vars.RequestVarsDebugPanel',
+    'debug_toolbar.panels.template.TemplateDebugPanel',
+    'debug_toolbar.panels.sql.SQLDebugPanel',
+    'debug_toolbar.panels.signals.SignalDebugPanel',
+    'debug_toolbar.panels.logger.LoggingPanel',
+)
