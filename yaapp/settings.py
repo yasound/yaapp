ccd1ea0ec9d2d3538de428bc17d3e2c5fd01d9fe
Jérôme Blondon <jerome@yasound.com>
2012-11-23 09:22:22 +0100

    reverted global broker for the moment

--- yaapp/settings.py
+++ yaapp/settings.py
@@ -175,9 +175,9 @@ elif DEVELOPMENT_MODE:
 elif PRODUCTION_MODE:
     # Celery config:
     CELERY_TASK_RESULT_EXPIRES = 18000  # 5 hours.
-    BROKER_URL = "redis://yas-sql-01:6379/0"
+    BROKER_URL = "redis://localhost:6379/0"
     CELERY_RESULT_BACKEND = "redis"
-    CELERY_REDIS_HOST = "yas-sql-01"
+    CELERY_REDIS_HOST = "localhost"
     CELERY_REDIS_PORT = 6379
     CELERY_REDIS_DB = 0
     CELERY_TASK_RESULT_EXPIRES = 10
