2d8ff5b6d477b7cda6aad82298cac6fa5d1be8b4
Jérôme BLONDON <jbl@yasound.com>
2012-12-05 13:30:22 +0100

    added dedicated https channel ur for deezer when not in deezer mode

--- yaapp/apps/yadeezer/urls.py
+++ yaapp/apps/yadeezer/urls.py
@@ -2,6 +2,7 @@ from django.conf.urls.defaults import *
 
 urlpatterns = patterns('yadeezer.views',
     url(r'^deezer/channel_url/$', 'channel_url', name='deezer_channel'),
+    url(r'^deezer/channel_url_https/$', 'channel_url_https', name='deezer_channel_https'),
     url(r'^deezer/communication/(?P<username>\S+)/$', 'deezer_communication', name='deezer_communication'),
 
     url(r'^api/v1/deezer/import_track/(?P<radio_uuid>\S+)/$', 'import_track'),
