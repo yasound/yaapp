0c8efdcc80d9fafb06886a0e863b2a7fe6d08c6b
Jérôme BLONDON <jbl@yasound.com>
2013-01-03 15:04:58 +0100

    teaser auto cycle

--- yaapp/media/js/app/teaser/views.js
+++ yaapp/media/js/app/teaser/views.js
@@ -15,7 +15,11 @@ Yasound.Views.Teaser = Backbone.View.extend({
     },
 
     initialize: function() {
-        _.bindAll(this, 'render');
+        _.bindAll(this, 'render', 'cycle', 'stopCycle', 'startCycle');
+    },
+
+    onClose: function () {
+        this.stopCycle();
     },
 
     render: function() {
@@ -23,6 +27,9 @@ Yasound.Views.Teaser = Backbone.View.extend({
             return;
         }
         this.$el.show();
+
+        this.cycleIntervalId = setInterval(this.cycle, 5*1000);
+
         return this;
     },
 
@@ -32,10 +39,14 @@ Yasound.Views.Teaser = Backbone.View.extend({
             cookies.set('hideteaser', true);
         }
         this.$el.slideUp();
+
+        this.close();
     },
 
     onArgumentChanged: function(e) {
         e.preventDefault();
+
+        this.stopCycle();
 
         var paginateLinks = this.$('.paginate a'),
             args = this.$('.argument'),
@@ -50,7 +61,43 @@ Yasound.Views.Teaser = Backbone.View.extend({
         $(paginateLinks[newIndex-1]).addClass('active');
         $(args[newIndex-1]).addClass('show');
         $(args[oldIndex-1]).removeClass('show');
-        
-    }
 
+        setTimeout(this.startCycle, 10*1000);
+    },
+
+    cycle: function () {
+        var paginateLinks = this.$('.paginate a');
+        var args = this.$('.argument');
+
+        var newIndex = paginateLinks.filter('.active').parent().next();
+
+        if (newIndex.length === 0) {
+            newIndex = this.$('.paginate a').first();
+        }
+
+        newIndex = newIndex.text();
+        var oldIndex = paginateLinks.filter('.active').text();
+
+        if(newIndex === oldIndex) {
+            return;
+        }
+
+        $(paginateLinks[oldIndex-1]).removeClass('active');
+        $(paginateLinks[newIndex-1]).addClass('active');
+        $(args[newIndex-1]).addClass('show');
+        $(args[oldIndex-1]).removeClass('show');
+
+    },
+
+    startCycle: function () {
+        this.stopCycle();
+        this.cycleIntervalId = setInterval(this.cycle, 5*1000);
+    },
+
+    stopCycle: function () {
+        if (this.cycleIntervalId) {
+            clearInterval(this.cycleIntervalId);
+            this.cycleIntervalId = undefined;
+        }
+    }
 });
